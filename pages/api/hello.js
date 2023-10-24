3639d0054e198ae38252415425fb7e467e590b30
ibrahem hassan <hibrahem266@gmail.com>
2023-10-24 13:13:18 +0300

    edit run time

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export const config = {
+  maxDuration: 300,
+};
+ 
+export default function handler(request, response) {
+  response.status(200).json({
+    body: request.body,
+    query: request.query,
+    cookies: request.cookies,
+  });
 }
