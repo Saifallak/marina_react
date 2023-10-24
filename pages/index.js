3064ffec06518eeb0c43d04f5d03cd19bf50da1f
ibrahem hassan <hibrahem266@gmail.com>
2023-10-24 16:50:48 +0300

    add load for data in home page

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,45 @@
-'use client'
+"use client";
 import Head from "next/head";
 import Image from "next/image";
 import HomeCard from "@/components/home/HomeCard";
 import styles from "@/styles/Home.module.scss";
 import Logo from ".././public/images/navbar/logo.png";
-import { Button, Container, Grid } from "@mantine/core";
+import { Button, Container, Grid, Skeleton } from "@mantine/core";
 import Link from "next/link";
 import { useTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Navbar from "../components/layouts/Navbar/index";
 import img from "@/public/images/home/hero.webp";
-import icon from '../public/Icon.png'
+import icon from "../public/Icon.png";
 import { useEffect, useState } from "react";
 import { getBlogs, getCatalog } from "@/components/useApi/dataApi";
 const { Col } = Grid;
 export default function Home() {
   const { t } = useTranslation("home");
   const { locale } = useRouter();
-  const [blogs,setBlogs] =useState([])
-  const [catalog,setCatalog] =useState([])
-  
-useEffect(()=>{
-  FetchDataOFBlogs()
-  FetchDataOFCatalog()
-},[])
+  const [blogs, setBlogs] = useState([]);
+  const [catalog, setCatalog] = useState([]);
+  const [Load1, setLoad1] = useState(false);
+  const [Load2, setLoad2] = useState(false);
+
+  useEffect(() => {
+    FetchDataOFBlogs();
+    FetchDataOFCatalog();
+  }, []);
   const FetchDataOFBlogs = async () => {
+    setLoad1(true);
     const Blogs = await getBlogs();
     if (!Blogs) console.log(Blogs?.message);
-   console.log(Blogs);
-   setBlogs(Blogs)
+    setBlogs(Blogs);
+    setLoad1(false);
   };
   const FetchDataOFCatalog = async () => {
+    setLoad2(true);
     const Catalog = await getCatalog();
     if (!Catalog) console.log(Catalog?.message);
-   console.log(Catalog);
-   setCatalog(Catalog)
+    setCatalog(Catalog);
+    setLoad2(false);
   };
   return (
     <>
@@ -44,23 +48,20 @@ useEffect(()=>{
         <meta name="description" content="Marina" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={icon.src} />
-       
       </Head>
       <Navbar />
       <main>
         <section className={styles.home}>
-          
-              <img
-                className={ " loaded"}
-                src={img.src}
-                alt="sea beach"
-                width={100}
-                height={100}
-                style={{ minHeight: "300px", height: "100%",width:"100%" }}
-              />
-            
+          <img
+            className={" loaded"}
+            src={img.src}
+            alt="sea beach"
+            width={100}
+            height={100}
+            style={{ minHeight: "300px", height: "100%", width: "100%" }}
+          />
 
-          <Container className={styles.home__container} fluid >
+          <Container className={styles.home__container} fluid>
             <div className={styles.home__title}>
               <p className={styles.discover}>{t("discover")}</p>
               <h1 className="mt-4 text-2xl leading-tight sm:text-4xl lg:text-6xl">
@@ -78,37 +79,46 @@ useEffect(()=>{
             </div>
           </Container>
         </section>
-        {
-          catalog.length>0? <section className={styles.looking__for}>
+        <section className={styles.looking__for}>
           <Container fluid px={20}>
             <h2 className="text-[#3a3a3a] text-2xl sm:text-6xl uppercase mb-6">
               {t("discoverDistance")}
             </h2>
-            <Grid className={styles.grid__container} columns={10}>
-              {catalog.map((item) => {
-                return (
-                  <Col span={2} key={item.id} className={styles.grid__grid}>
-                    <Link href={`/whereToGo/${item.id}`}>
-                      <div className={`${styles.grid__item} cursor-pointer `}>
-                        {(
-                          <Image
-                            className={styles.imgLook}
-                            src={item.img_collection.responsive_urls[0]}
-                            fill
-                            srcSet={item.img_collection.responsive_urls}
-                            loading="lazy"
-                            decoding="async"
-                            sizes="100%"
-                            alt={item.name[locale]}
-                          />
-                        ) || <Skeleton />}
-                        <h3>{item.name[locale]}</h3>
-                      </div>
-                    </Link>
-                  </Col>
-                );
-              })}
-            </Grid>
+           {Load2&&<div className="loadDiv">
+              <Skeleton height={300} width={"22%"} radius="8px" />
+              <Skeleton height={300} width={"22%"} radius="8px" />
+              <Skeleton height={300} width={"22%"} radius="8px" />
+              <Skeleton height={300} width={"22%"} radius="8px" />
+              <Skeleton height={300} width={"22%"} radius="8px" />
+              <Skeleton height={300} width={"22%"} radius="8px" />
+            </div>}
+            {catalog.length > 0 ? (
+              <Grid className={styles.grid__container} columns={10}>
+                {catalog.map((item) => {
+                  return (
+                    <Col span={2} key={item.id} className={styles.grid__grid}>
+                      <Link href={`/whereToGo/${item.id}`}>
+                        <div className={`${styles.grid__item} cursor-pointer `}>
+                          {(
+                            <Image
+                              className={styles.imgLook}
+                              src={item.img_collection.responsive_urls[0]}
+                              fill
+                              srcSet={item.img_collection.responsive_urls}
+                              loading="lazy"
+                              decoding="async"
+                              sizes="100%"
+                              alt={item.name[locale]}
+                            />
+                          ) || <Skeleton />}
+                          <h3>{item.name[locale]}</h3>
+                        </div>
+                      </Link>
+                    </Col>
+                  );
+                })}
+              </Grid>
+            ) : null}
             {/*<div className={styles.see__more}>
               <Link href={"/whatToDo"}>
                 <Button uppercase className={styles.btn}>
@@ -117,25 +127,31 @@ useEffect(()=>{
               </Link>
             </div>*/}
           </Container>
-        </section>:null
-        }
-       
-        {
-          blogs.length>0? <Container fluid px={20}>
-          <section>
-            {blogs.map((item, i) => (
-              <HomeCard
-                key={i}
-                title={item.title[locale]}
-                image={item.img_collection.responsive_urls}
-                id={item.id}
-                t={t}
-                reverse={i % 2 == 0 ? "" : "true"}
-              />
-            ))}
-          </section>
-        </Container>:null
-        }
+        </section>
+
+        
+          <Container fluid px={20}>
+            <section>
+            {Load1&&<div className="loadDiv" style={{marginTop:"50px"}}>
+              <Skeleton height={300} width={"90%"} radius="8px" />
+              <Skeleton height={300} width={"90%"} radius="8px" />
+              <Skeleton height={300} width={"90%"} radius="8px" />
+              
+            </div>}
+            {blogs.length > 0 ? (
+              blogs.map((item, i) => (
+                <HomeCard
+                  key={i}
+                  title={item.title[locale]}
+                  image={item.img_collection.responsive_urls}
+                  id={item.id}
+                  t={t}
+                  reverse={i % 2 == 0 ? "" : "true"}
+                />
+              ))
+              ) : null}
+            </section>
+          </Container>
        
 
         <Container fluid px={20}>
@@ -154,7 +170,6 @@ useEffect(()=>{
           <Link className={styles.footer_logo} href="/">
             <Image src={Logo} alt="logo"></Image>
           </Link>
-       
         </section>
       </main>
     </>
---
+++ pages/loading.js
@@ -0,0 +1,48 @@
+import { DEFAULT_THEME, LoadingOverlay } from '@mantine/core';
+import { Router } from 'next/router';
+import { useState, useEffect } from 'react';
+
+export default function Loading() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    Router.events.on('routeChangeStart', handleStart);
+    Router.events.on('routeChangeComplete', handleComplete);
+    Router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      Router.events.off('routeChangeStart', handleStart);
+      Router.events.off('routeChangeComplete', handleComplete);
+      Router.events.off('routeChangeError', handleComplete);
+    };
+  }, []);
+  const customLoader = (
+    <svg
+      width="54"
+      height="54"
+      viewBox="0 0 38 38"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke={DEFAULT_THEME.colors.blue[6]}
+    >
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(1 1)" strokeWidth="3">
+          <circle strokeOpacity="0.1" cx="18" cy="18" r="18" />
+          <path d="M36 18c0-9.94-8.06-18-18-18">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 18 18"
+              to="360 18 18"
+              dur="1s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </g>
+      </g>
+    </svg>
+  );
+  return  loading?  <LoadingOverlay className='load'  loader={customLoader} overlayBlur={10} visible />  :null;
+}
